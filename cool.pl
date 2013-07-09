c20f96d9a979f2b39db0b35f17856563aebd94b1
Seth D. Galitzer <sethgali@gmail.com>
2013-07-09 12:52:41 -0500

    added a new silly script

---
+++ cool.pl
@@ -0,0 +1,41 @@
+use strict;
+use Encode;
+use vars qw($VERSION %IRSSI);
+use Irssi;
+
+our $VERSION = '0.1';
+our %IRSSI = (
+    authors     => 'Seth Galitzer',
+    contact     => 'sethgali@gmail.com',
+    name        => 'notcool',
+    description => 'silly response to !cool',
+    license     => 'Public Domain',
+);
+
+sub handler {
+    my ($server, $msg, $nick, $addr, $target, $priv) = @_;
+
+    use utf8;
+    $msg = decode_utf8 $msg;
+    if ($msg =~ m/^!cool/) {
+        $msg = encode_utf8("that is so not cool");
+        if ($priv) {
+            $server->command ("msg $nick $msg");
+        } else {
+            $server->command ("msg $target $msg");
+        }
+    }
+}
+
+Irssi::signal_add_last('message public', sub {
+    my ($server, $msg, $nick, $addr, $target) = @_;
+    Irssi::signal_continue($server, $msg, $nick, $addr, $target);
+    handler($server, $msg, $nick, $addr, $target);
+});
+
+Irssi::signal_add_last('message private', sub {
+    my ($server, $msg, $nick, $addr, $target) = @_;
+    Irssi::signal_continue($server, $msg, $nick, $addr, $target);
+    handler($server, $msg, $nick, $addr, $target, 1);
+});
+
