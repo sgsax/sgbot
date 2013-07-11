a5dd96fd3af5a19e573f586c11e0bf494d23245c
Seth D. Galitzer <sethgali@gmail.com>
2013-07-11 09:28:36 -0500

    added new feature

---
+++ renick.pl
@@ -0,0 +1,37 @@
+use strict;
+use Encode;
+use vars qw($VERSION %IRSSI);
+use Irssi;
+
+our $VERSION = '0.1';
+our %IRSSI = (
+    authors     => 'Seth Galitzer',
+    contact     => 'sethgali@gmail.com',
+    name        => 'renick',
+    description => 'allow one person to remotely trigger a renick of the bot',
+    license     => 'Public Domain',
+);
+
+sub handler {
+    my ($server, $msg, $nick, $addr, $target, $priv) = @_;
+
+    use utf8;
+    $msg = decode_utf8 $msg;
+    if (($msg =~ m/^!renick/) && ($nick eq 'sgsax')) {
+        $msg = encode_utf8('/nick sgbot');
+        $server->command ("$msg");
+    }
+}
+
+Irssi::signal_add_last('message public', sub {
+    my ($server, $msg, $nick, $addr, $target) = @_;
+    Irssi::signal_continue($server, $msg, $nick, $addr, $target);
+    handler($server, $msg, $nick, $addr, $target);
+});
+
+Irssi::signal_add_last('message private', sub {
+    my ($server, $msg, $nick, $addr, $target) = @_;
+    Irssi::signal_continue($server, $msg, $nick, $addr, $target);
+    handler($server, $msg, $nick, $addr, $target, 1);
+});
+
