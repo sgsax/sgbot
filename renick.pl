262d2e1f87af4715c6cf263b396ecd238d11d079
Seth D. Galitzer <sethgali@gmail.com>
2013-07-11 09:32:00 -0500

    fix command

--- renick.pl
+++ renick.pl
@@ -18,7 +18,7 @@ sub handler {
     use utf8;
     $msg = decode_utf8 $msg;
     if (($msg =~ m/^!renick/) && ($nick eq 'sgsax')) {
-        $msg = encode_utf8('/nick sgbot');
+        $msg = encode_utf8("nick sgbot");
         $server->command ("$msg");
     }
 }
