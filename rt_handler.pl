aa2cf5df3123c49f6bcc45c9daad0939fa5d1dbb
Seth D. Galitzer <sethgali@gmail.com>
2014-01-23 12:25:50 -0600

    added more commands for rt ticket handling

---
+++ rt_handler.pl
@@ -0,0 +1,81 @@
+use strict;
+use Encode;
+use vars qw($VERSION %IRSSI);
+use Irssi;
+
+our $VERSION = '0.1';
+our %IRSSI = (
+    authors     => 'Seth Galitzer',
+    contact     => 'sethgali@gmail.com',
+    name        => 'rt_lookup',
+    description => 'looks up a ticket in RT by a provided number and returns the subject and url',
+    license     => 'Public Domain',
+);
+
+sub do_lookup {
+    my $tkt = shift;
+    my $ret;
+    my @result=`rt show -t ticket -f subject ticket/$tkt`;
+    my $subj = $result[1];
+    if (!($subj =~ m/^$/)) {
+        chomp($subj);
+        $subj =~ s/Subject/\#$tkt/;
+        $ret = "$subj - https://rt4.cis.ksu.edu/rt/Ticket/Display.html?id=$tkt";
+    } else {
+        $ret = "Ticket \#$tkt not found";
+    }
+
+    return $ret;
+}
+
+sub do_cmd {
+    my $tkt = shift;
+    my $cmd = shift;
+
+    my $ret;
+
+    my @result=`rt $cmd $tkt`;
+    $ret = $result[1];
+
+    return $ret;
+}
+
+sub handler {
+    my ($server, $msg, $nick, $addr, $target, $priv) = @_;
+
+    use utf8;
+
+    my $ret = "";
+    $msg = decode_utf8 $msg;
+    if ($target eq '#ksucis-dudes') {
+        if ($msg =~ m/#(\d+)\b/) {
+            $ret = encode_utf8(do_lookup($1));
+        } elsif (($msg =~ /^!del\s/)||($msg =~ /^!delete\s/)||
+                ($msg =~ /^!res\s/)||($msg =~ /^!resolve\s/)) {
+            my @parts = split(/\s+/, $msg);
+            my $cmd = $parts[0];
+            $cmd =~ s/!//;
+            $ret = encode_utf8(do_cmd($parts[1], $cmd));
+        }
+        if ($ret ne "") {
+            if ($priv) {
+                $server->command ("msg $nick $msg");
+            } else {
+                $server->command ("msg $target $msg");
+            }
+        }
+    }
+}
+
+Irssi::signal_add_last('message public', sub {
+    my ($server, $msg, $nick, $addr, $target) = @_;
+    Irssi::signal_continue($server, $msg, $nick, $addr, $target);
+    handler($server, $msg, $nick, $addr, $target);
+});
+
+Irssi::signal_add_last('message private', sub {
+    my ($server, $msg, $nick, $addr, $target) = @_;
+    Irssi::signal_continue($server, $msg, $nick, $addr, $target);
+    handler($server, $msg, $nick, $addr, $target, 1);
+});
+
