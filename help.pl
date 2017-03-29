983e906d5797dff47348fa7918a572131e6a6087
Seth D. Galitzer <sethgali@gmail.com>
2017-03-29 09:21:04 -0500

    move help to github wiki page and send link to that instead of spamming msg to user

--- help.pl
+++ help.pl
@@ -3,7 +3,7 @@ use Encode;
 use vars qw($VERSION %IRSSI);
 use Irssi;
 
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 our %IRSSI = (
     authors     => 'Seth Galitzer',
     contact     => 'sethgali@gmail.com',
@@ -11,51 +11,6 @@ our %IRSSI = (
     description => 'canonical help for all bot commands',
     license     => 'Public Domain',
 );
-sub list_help {
-
-    my @commands = ( "Available commands for sgbot follow (prefix with !):",
-                     "8ball: The Magic 8 Ball",
-                     "bofh: Display witty fortunes for sysadmins",
-                     "calc <formula>: Simple, in-line calculator, evaluates <formula> and returns the answer",
-                     "cool: silly response to this statement",
-                     "doh: play Homer sound",
-                     "donut <name>: tell <name> to go and take a flying fuck at a rolling donut (FOAAS)",
-                     "everything: fuck everything (FOAAS)",
-                     "everyone: everyone can go and fuck off (FOAAS)",
-                     "flip: flip a coin",
-                     "google: google that shit",
-                     "gtfo: time to go",
-                     "help: Display this help",
-                     "holy: holy fuck (FOAAS)",
-                     "idm: When is it IDM time?",
-                     "kahn: play Khan sound",
-                     "linus: witty fuck off that only Linus Torvalds could say (FOAAS)",
-                     "no: fuck no (FOAAS)",
-                     "off <name>: tell <name> to fuck off (FOAAS)",
-                     "rfo [<name>]: random FOAAS selection (FOAAS)",
-                     "rimshot: print url for rimshot sound",
-                     "roll: roll the dice, choose d4, d6 (default), d8, d10, d12, or d20",
-                     "roy: every tech support call ever",
-                     "rum: say something piratey",
-                     "spell <word>: Simple spellchecker, looks up <word> and suggests replacements if needed",
-                     "tada: print url for tada sound",
-                     "tequila: drinks are on me",
-                     "this: fuck this (FOAAS)",
-                     "that: fuck that (FOAAS)",
-                     "train <name>: all aboard the fail train",
-                     "trombone: print url for sad trombone sound",
-                     "up <name>: <name> really fucked up (FOAAS)",
-                     "USA: wouldn't it be great?",
-                     "vodka: just like from the old country",
-                     "weather [<location>]: Display weather report for <location> or MHK if not indicated",
-                     "whiskey: for those really difficult days",
-                     "woot [img] [buy]: Display today's Woot.com special, optionally include image and/or \"buy now\" URL",
-                     "yeah <name>: fuck yeah! (FOAAS)",
-                     "you <name>: tell <name>, fuck you (FOAAS)",
-                     );
-
-    return @commands;
-}
 
 sub handler {
     my ($server, $msg, $nick, $addr, $target, $priv) = @_;
@@ -64,9 +19,7 @@ sub handler {
 
     $msg = decode_utf8 $msg;
     if ($msg =~ m/^!help$/) {
-        foreach my $command (list_help()) {
-            $server->command (encode_utf8("msg $nick $command"));
-        }
+        $server->command (encode_utf8("msg $nick You can find sgbot help here: https://github.com/sgsax/sgbot/wiki/Help"));
     }
 }
 
