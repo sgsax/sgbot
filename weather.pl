e00c7a437f1bc588195fee8bdee75b8734665c7a
Seth D. Galitzer <sethgali@gmail.com>
2020-11-12 12:04:07 -0600

    update weather script to use openweathermap.org

--- weather.pl
+++ weather.pl
@@ -2,8 +2,9 @@ use strict;
 use Encode;
 use vars qw($VERSION %IRSSI);
 use Irssi;
+use JSON;
 
-our $VERSION = '0.2';
+our $VERSION = '0.3';
 our %IRSSI = (
     authors     => 'Seth Galitzer',
     contact     => 'sethgali@gmail.com',
@@ -12,12 +13,37 @@ our %IRSSI = (
     license     => 'Public Domain',
 );
 
+our %icons = (
+    "01" => "\x{2600}",
+    "02" => "\x{1F324}",
+    "03" => "\x{2601}",
+    "04" => "\x{1F325}",
+    "09" => "\x{1F326}",
+    "10" => "\x{1F327}",
+    "11" => "\x{26C8}",
+    "13" => "\x{2744}",
+    "50" => "\x{1F32B}"
+);
+
+sub get_secrets {
+    use File::Slurp;
+    
+    # The secrets.json file should be placed in the same directory as this
+    #   script and should be formatted as follows:
+    #   { "apikey" : "youropenweathermapapikey" }
+    my $secrets = read_file("./secrets.json");
+    return decode_json($secrets);
+}
+
 sub get_the_weather {
-    use XML::Simple;
     use LWP::Simple;
     use URI::Escape;
     ##use Data::Dumper;
  
+    my $apikey = get_secrets()->{apikey};
+
+    my $ret;
+
     # User variable, change this to suit your location
     my $location = shift;
     if ($location eq "") { $location = "66502"; };
@@ -26,51 +52,39 @@ sub get_the_weather {
     $location = uri_escape($location);
  
     # get the current conditions
-    my $output_current = get("http:\/\/api.wunderground.com\/auto\/wui\/geo\/WXCurrentObXML\/index.xml?query=$location");
-    # get forecast data
-    my $output_forecast = get("http:\/\/api.wunderground.com\/auto\/wui\/geo\/ForecastXML\/index.xml?query=$location");
- 
-    my $data_current;
-    my $data_forecast;
-    my $ret;
 
-    # parse retrieved data and dump XML to hash reference
-    my $xml = new XML::Simple;
-    if (defined $output_current) {
-        $data_current = $xml->XMLin($output_current);
-    }
-    if (defined $output_forecast) {
-        $data_forecast = $xml->XMLin($output_forecast);
-    }
-    ##print Dumper($data_current);
-    ##print Dumper($data_forecast);
-    
-    if ($data_current->{display_location}->{full} eq ", ") {
+    my $raw = get("http:\/\/api.openweathermap.org\/data\/2.5\/weather?zip=$location&units=imperial&appid=$apikey");
+    if ($raw eq ""){
         return "Invalid location code: $location\n";
-    }
-    
+    };
+    my $current = decode_json($raw);
+
+    my $lat = $current->{coord}->{lat};
+    my $lon = $current->{coord}->{lon};
+
+    # get forecast data
+    my $forecast = decode_json(get("https:\/\/api.openweathermap.org\/data\/2.5\/onecall?lat=$lat&lon=$lon&exclude=current,minutely,hourly,alerts&units=imperial&appid=$apikey"));
+ 
     # display data
-    if (not defined $output_current) {
+    if (not defined $current) {
         $ret = "Current weather not available";
     } else {
-        $ret = "Current weather for $data_current->{display_location}->{full} | ";
-        $ret .= "$data_current->{observation_time} | ";
-        $ret .= "$data_current->{weather}, Temp: $data_current->{temp_f}\x{b0}F,";
-        if ($data_current->{heat_index_f} ne 'NA') {
-            $ret .= " Heat Index: $data_current->{heat_index_f}\x{b0}F | ";
-        } else {
-            $ret .= " Wind Chill: $data_current->{windchill_f}\x{b0}F | ";
-        };
-        $ret .= "Humidity: $data_current->{relative_humidity}, Pressure: $data_current->{pressure_in}\" | ";
-        $ret .= "Wind: $data_current->{wind_dir} $data_current->{wind_mph}mph | ";
+        $ret = "Current weather for $current->{name} | ";
+        $ret .= localtime($current->{dt}) . " | ";
+        $ret .= "$current->{weather}[0]->{description} ";
+        $ret .= $icons{substr($current->{weather}[0]->{icon}, 0, 2)} . ", ";
+        $ret .= "Temp: $current->{main}->{temp}\x{b0}F, ";
+        $ret .= "Feels like: $current->{main}->{feels_like}\x{b0}F | ";
+        $ret .= "Humidity: $current->{main}->{humidity}%, Pressure: $current->{main}->{pressure} hPa | ";
+        $ret .= "Wind: $current->{wind}->{deg} $current->{wind}->{speed} mph | ";
     }
 
-    if (not defined $output_forecast) {
+    if (not defined $forecast) {
         $ret .= "Forecast not available";
     } else {
-        $ret .= "Forecast: $data_forecast->{simpleforecast}->{forecastday}[1]->{conditions} | ";
-        $ret .= "High: $data_forecast->{simpleforecast}->{forecastday}[1]->{high}->{fahrenheit} F,";
-        $ret .= " Low: $data_forecast->{simpleforecast}->{forecastday}[1]->{low}->{fahrenheit} F";
+        $ret .= "Forecast: $forecast->{daily}[0]->{weather}[0]->{description} | ";
+        $ret .= "High: $forecast->{daily}[0]->{temp}->{max}\x{b0}F,";
+        $ret .= " Low: $forecast->{daily}[0]->{temp}->{min}\x{b0}F";
     }
 
     return $ret;
